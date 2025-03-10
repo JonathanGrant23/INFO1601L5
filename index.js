2fb1968cd73a245c221a55159be7b55bc8ef7173
JonathanGrant23 <jonathangrant360@gmail.com>
2025-03-10 10:30:36 -0400

    All Lab 5 tasks are completed!

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ console.log(`Hello my name is ${name}, I'm ${age} years old`);
 console.log(`I was born in ${2020 - age}`)
 //I was born in 1966
 
+//Task 4.1
+console.log('1' == 1); //true
+console.log(1 == true); //true
+console.log("false" == false); // "false" is not false
+console.log("false" == true); // "false" is not true either
+
+if("false")
+   console.log("Hello false!") //but "false" is true enough here
+
+//Task 4.2
+console.log('1' === 1); //false
+console.log(1 === true); //false
+console.log("true" === true); //false
+
 //Task 4.3
 for(let i=0; i< 50; i++){
   if(i %3 === 0 && i%5 === 0){ // is divisible by 3 and 5?
@@ -84,3 +98,68 @@ console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
 let reversed = arr.reverse();//creates a new array in reverse order 
 console.log(reversed);//[344, 103, 42, 33, 16, -5]
 console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
+
+//Task 5.2
+let arr1 = [12, 33, 4, 5, -4, 8, 19, 25];
+                
+//map() creates a new array from the elements of one without changing the old one
+function double(num){
+  return num * 2;
+}
+
+let doubledArr = arr.map(double); 
+console.log(doubledArr);
+
+function isOdd(num){
+  return element%2 !== 0; 
+}
+//Filter takes a test condition and returns only the element which 
+//make the condition true
+let odds = arr.filter(isOdd);
+console.log(odds);
+
+//Returns true or false if any of the elements of the array 
+//meets a specified condition
+function has5Factor(){
+  return ele % 5 === 0;
+}
+
+let hasFiveFactor = arr.some(has5Factor);
+console.log(hasFiveFactor);
+
+function intCompare(a, b){
+ return a - b;
+}
+
+//sort function must return either 0, +ve, -ve
+let ascending = arr.sort(intCompare)
+console.log(ascending);
+
+//Task 6
+//Create a constructor a functions which builds object for us
+function createPerson(name, height, weight) {
+  return { name: name, height: height, weight: weight };
+}
+
+function calcBMI(weight, height) {
+  return weight / (height * height);
+}
+
+function avgBMI(people) {
+  let sum = 0;
+  for (let person of people) {
+    //sum the bmi of each person
+    sum += calcBMI(person.weight, person.height);
+  }
+  //calculate average
+  return sum / people.length;
+}
+
+//create a collection of people
+let people = [
+  createPerson("Sally", 60, 2.5),
+  createPerson("Ben", 81, 3),
+  createPerson("Shelly", 50, 1.7)
+];
+
+console.log(avgBMI(people));
